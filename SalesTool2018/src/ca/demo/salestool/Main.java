956365b1d7e63693a56abdd7e8dfde864c3feec7
samir <samir@192.168.1.126>
2019-02-21 14:42:51 -0600

    salam added

--- SalesTool2018/src/ca/demo/salestool/Main.java
+++ SalesTool2018/src/ca/demo/salestool/Main.java
@@ -14,6 +14,7 @@ public class Main {
 		System.out.println("THIS APP SHOWS SALES DATA");
 		System.out.println("Woot! It pushed!");
 		System.out.println("samir");
+		System.out.println("salam");
 	}
 	
 }
